f691ebd50ec67ebc49f53139365b7ed3a9fa04ee
SimonHanzal <71489526+SimonHanzal@users.noreply.github.com>
2020-10-22 11:28:06 +0100

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -1,15 +1,14 @@
-
 	/* METADATA */
 	
 	var timeline = [];
 	var no_trials = 2;
-	var repo_site = "https://simonhanzal.github.io/prototype/";
+	var repo_site = "https://simonhanzal.github.io/RT/";
 
     /* WELCOME */
     
 	var welcome = {
       type: "html-keyboard-response",
-      stimulus: "Welcome to the prototype experiment. Please wait a few seconds and then press any key to begin."
+      stimulus: "Welcome to the prototype experiment. Press any key to begin."
     };
     timeline.push(welcome);
 
@@ -20,11 +19,11 @@
       stimulus: "<p>In this experiment, a number will appear in the center " +
           "of the screen.</p><p>If the number is <strong>3</strong>, " +
           "do not press anything.</p>" +
-          "<p>If it is any other number between <strong>0-2</strong> or <strong>4-9</strong>, press the Space Bar " +
+          "<p>If it is any other number between <strong>0-2</strong> or <strong>4-9</strong>, press the <strong>Space Bar</strong> " +
           "as fast as you can.</p>" +
 	  "<p></p>" +
-	  "<p>Make sure the experiment is open in an active window, displayed in the center, your keyboard is connected to "+
-	  "the computer and that there are no distractions around you.</p>" +
+	  "<p>Make sure the experiment is open in an active window, displayed in the center, you are using a computer,</p>"+
+	  "<p>your keyboard is connected to the computer and that there are no distractions around you.</p>" +
 	  "<p></p>" +	
           "<p>Press any key to begin.</p>",
       post_trial_gap: 2000
@@ -49,7 +48,7 @@
       type: 'html-keyboard-response',
       stimulus: "<p></p>",//'<div style="font-size:60px;">+</div>',
       choices: jsPsych.NO_KEYS,
-      trial_duration: 15,
+      trial_duration: 50,
       data: {test_part: 'fixation'}
     }
 
@@ -58,16 +57,21 @@
       stimulus: jsPsych.timelineVariable('stimulus'),
       choices: ['f1','Space'],
 	  stimulus_duration: 250,
-	  trial_duration: 1135,
+	  trial_duration: 1100,
 	  response_ends_trial: false,
       data: jsPsych.timelineVariable('data'),
 	  on_finish: function(data){
 		data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
 		data.three = data.stimulus == '<div style="font-size:60px;">3</div>';
 		if(data.correct == true){
-		  data.type == 1;
+		  data.type = 1;
 		} else {
-		  data.type == 0
+		  data.type = 0;
+	       }
+		if(data.stimulus == '<div style="font-size:60px;">3</div>'){
+		  data.go = 3;
+		} else {
+		  data.go = 2;
 	       }
 	},
     }
@@ -104,31 +108,45 @@
 		var minimum = Math.round(correct_go_trials.select('rt').min());
 		var maximum = Math.round(correct_go_trials.select('rt').max());
 		
+		function roundme(value) {
+		return Math.round(value);
+		}
+		
 		var trial_data1 = Object.entries(correct_go_trials.select('rt'));
 		var trial_data2 = trial_data1.slice(0,1).toString();
 		var trial_data3 = trial_data2.split(",");
-		var trial_data_array = trial_data3.slice(1);
-		var trial_data_word = trial_data_array.join(";");
-		    
+		var trial_data4 = trial_data3.slice(1);
+		var trial_data5 = trial_data4.map(roundme);
+		var trial_data_word = trial_data5.join(";");
+		
 		var trial_value1 = Object.entries(trials.select('type'));
-		var trial_value2 = trial_value1.join(";");
+		var trial_value2 = trial_value1.slice(0,1).toString();
+		var trial_value3 = trial_value2.split(",");
+		var trial_value4 = trial_value3.slice(1);
+		var trial_value5 = trial_value4.join(";");
+		
+		var trial_go1 = Object.entries(trials.select('go'));
+		var trial_go2 = trial_go1.slice(0,1).toString();
+		var trial_go3 = trial_go2.split(",");
+		var trial_go4 = trial_go3.slice(1);
+		var trial_go5 = trial_go4.join(";");
 		
 		var rt = Math.round(correct_trials.select('rt').mean());
 		var true_rt = Math.round(correct_go_trials.select('rt').mean()*100)/100;
 		var no_go_rt = Math.round(correct_relevant.select('rt').mean());
-		var sd1 = trial_data_array.map(fc);
+		var sd1 = trial_data4.map(fc);
 
-		 function fc(value) {
+		function fc(value) {
 		const mean = true_rt
 		return (value - true_rt) ** 2;
 		}
+		
 		var sd2 = sd1.reduce((a,b) => a + b, 0);
 		var sd3 = sd2/sd1.length;
 		var sd4 = Math.sqrt(sd3);
 		var deviation = Math.round(sd4*100)/100;
-		
 			
-			return "<p>PRESS ANY KEY TO "+ trial_value2 +"CONTINUE!</p>";
+			return "<p>Press any key to complete the experimental part. Thank you!</p>";
 		
 		}
 	};
